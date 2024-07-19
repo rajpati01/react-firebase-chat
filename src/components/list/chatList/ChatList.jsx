a9827f88dbd7c487783550c208034b08aac93f75
rajpati <rajpatibhandari01@gmail.com>
2024-07-19 15:04:05 +0545

    completed ChatList component

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -13,8 +13,37 @@ function ChatList() {
           </div>
           <img src={addMode ? "./minus.png" : "./plus.png"} alt="" className='add' onClick={() => setAddMode((prev) => !prev)} />
         </div>
+
+        <div className="item">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Rajpati</span>
+            <p>Hello World!</p>
+          </div>
+        </div>
+        <div className="item">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Rajpati</span>
+            <p>Hello World!</p>
+          </div>
+        </div>
+        <div className="item">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Rajpati</span>
+            <p>Hello World!</p>
+          </div>
+        </div>
+        <div className="item">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Rajpati</span>
+            <p>Hello World!</p>
+          </div>
+        </div>
       </div>     
-  )
-}
+  );
+};
 
 export default ChatList
