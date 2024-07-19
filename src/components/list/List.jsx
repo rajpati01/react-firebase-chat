5d6818f41ef220c4b3828ce9fddbe93730e18378
rajpati <rajpatibhandari01@gmail.com>
2024-07-19 13:50:33 +0545

    updated the chiltList component

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,9 +1,14 @@
 import './list.css'
 import React from 'react'
+import UserInfo from './userInfo/UserInfo'
+import ChatList from './chatList/ChatList'
 
 function List() {
   return (
-    <div className='list'>List</div>
+    <div className='list'>
+      <UserInfo />
+      <ChatList />
+    </div>
   )
 }
 
