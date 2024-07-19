4677825471bca4cb6b3f26ec4fdb7e0affcd9869
rajpati <rajpatibhandari01@gmail.com>
2024-07-19 16:16:29 +0545

    updated chat component adding user info

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -3,7 +3,37 @@ import React from 'react'
 
 function Chat() {
   return (
-    <div className='chat'>Chat</div>
+    <div className='chat'>
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Rajpati</span>
+            <p>Lorem ipsum dolor sit.</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
+        </div>
+      </div>
+
+      <div className="center"></div>
+
+      <div className="bottom">
+        <div className="icons">
+          <img src="./img.png" alt="" />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+          <input type="text" placeholder='Type a message..' />
+          <div className="emoji">
+            <img src="./emoji.png" alt="" />
+          </div>
+        <button className='sendButton'>Send</button>
+      </div>
+    </div>
   )
 }
 
