7efadb65387b9b3ab56fde010cacc46ff8a318a8
rajpati <rajpatibhandari01@gmail.com>
2024-07-20 16:56:18 +0545

    update detail component

--- src/components/details/Detail.jsx
+++ src/components/details/Detail.jsx
@@ -3,7 +3,67 @@ import React from 'react'
 
 function Detail() {
   return (
-    <div className='detail'>Detail</div>
+    <div className='detail'>
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>rajpati</h2>
+        <p>Lorem ipsum dolor sit.</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy & help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+            <div className="photoItem"> 
+              <div className="photoDetail">
+                  <img src="https://images.pexels.com/photos/15898047/pexels-photo-15898047/free-photo-of-surfer-running-towards-ocean.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+                    alt="" />
+                  <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" className='icon'/>
+            </div>
+            <div className="photoItem"> 
+              <div className="photoDetail">
+                  <img src="https://images.pexels.com/photos/15898047/pexels-photo-15898047/free-photo-of-surfer-running-towards-ocean.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+                    alt="" />
+                  <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" className='icon'/>
+            </div>
+            <div className="photoItem"> 
+              <div className="photoDetail">
+                  <img src="https://images.pexels.com/photos/15898047/pexels-photo-15898047/free-photo-of-surfer-running-towards-ocean.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+                    alt="" />
+                  <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" className='icon'/>
+            </div>
+          </div>
+        </div>
+
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block User</button>
+      </div>
+    </div>
   )
 }
 
